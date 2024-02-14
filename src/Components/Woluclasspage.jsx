2771cb43bad093c2280bf9899cac2abff033cfba
bli farda <134302929+farda222@users.noreply.github.com>
2024-02-14 17:23:01 +0700

    Update Woluclasspage.jsx

--- src/Components/Woluclasspage.jsx
+++ src/Components/Woluclasspage.jsx
@@ -139,7 +139,7 @@ const LoginPage = ({ onLogin }) => {
   };
 
   return (
-    <form className="flex flex-col items-center justify-center h-screen bg-gray-400 font-custom px-4 py-2">
+    <form className="flex flex-col items-center justify-center h-screen bg-gray-400 font-custom px-4 py-2 overflow-y-hidden">
       <div className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 w-full max-w-md mt-4">
         <div className="mb-3 flex justify-center mt-2">
           <img src={Logo} alt="Logo" className="h-8 w-auto" />
